e2d9feacbb4a72514233e13abf29756c1cc0bd8f
EkaterinaTadueva <etadueva@bk.ru>
2023-04-23 18:03:03 +0300

    Готовая сборка

--- webpack.config.js
+++ webpack.config.js
@@ -1,17 +1,88 @@
 const path = require('path');
+const HTMLWebpackPlugin = require('html-webpack-plugin');
+const {CleanWebpackPlugin} = require('clean-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const WebpackDevServer = require('webpack-dev-server');
+const CssMinimizerPlugin = require("css-minimizer-webpack-plugin");
+const TerserPlugin = require("terser-webpack-plugin");
+
+
+const isDev = process.env.NODE_ENV === 'development';
+const isProd = !isDev;
+
+const filename = (ext) => isDev ? `[name].${ext}` : `[name].[contenthash].${ext}`;
 
 module.exports = {
-  entry: './src/index.js',
+  context: path.resolve(__dirname, 'src'),
+  mode: 'development',
+  entry: './js/main.js',
   output: {
-    filename: 'main.js',
-    path: path.resolve(__dirname, 'dist'),
+    filename: `./js/${filename('js')}`,
+    path: path.resolve(__dirname, 'dist')
   },
+  plugins: [
+    new HTMLWebpackPlugin({
+      template: path.relative(__dirname, 'index.html'),
+      filename: 'index.html',
+      minify: {
+        collapseWhitespace: isProd
+      },
+      inject: true
+    }),
+    new CleanWebpackPlugin(),
+    new MiniCssExtractPlugin({
+      filename: `./css/${filename('css')}`,
+      chunkFilename: '[id].css',
+    })
+  ],
   module: {
     rules: [
       {
-        test: /\.css$/i,
-        use: ['style-loader', 'css-loader'],
+        test: /\.html$/,
+        loader: "html-loader",
       },
+      {
+        test: /\.css$/i,
+        use: [MiniCssExtractPlugin.loader, "css-loader"],
+      },
+      {
+        test: /\.(?:|gif|png|jpg|jpeg|svg)$/i,
+        type: 'asset/resource',
+        generator: {
+          publicPath: './img/',
+          outputPath: './img/',
+        },
+      },
+      {
+        test: /\.(woff|woff2)$/i,
+        type: 'asset/resource',
+        generator: {
+          publicPath: './fonts/',
+          outputPath: './fonts/',
+        },
+      },
+      {
+        test: /\.js$/i,
+        exclude: /node_modules/,
+        use: ['babel-loader']
+      },
+    ]
+  },
+  devServer: {
+    historyApiFallback: true,
+    static: {
+      directory: path.join(__dirname, 'dist'),
+    },
+    open: true,
+    compress: true,
+    hot: true,
+    port: 3000,
+  },
+  optimization: {
+    minimizer: [
+      new CssMinimizerPlugin(),
+      new TerserPlugin()
     ],
+    minimize: true,
   },
 };
